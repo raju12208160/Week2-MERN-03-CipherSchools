60db1cdb96860eff67e59e139786e59c6e726185
Gudipati Raju <mailraju1406@gmail.com>
2024-07-19 23:06:29 +0530

    Delete Lecture 25/Node directory

--- Lecture 25/Node/modulePattern/App.js
+++
@@ -1,8 +0,0 @@
-var stuffs =require( './provider');
-//console.log(stuffs);
-
-let arr=[1,2,3,4,5,6,7,8,9,0]
-console.log(stuffs.counter(arr));
-console.log(stuffs.addition(arr[7],arr[4]));
-
-console.log(stuffs.addition.constant, arr[4]);
